0b584e6bad5d620a939d4193e60ce37c00013775
Adrian400811 <adrianleeho@gmail.com>
2023-05-31 09:04:24 -0400

    Changed game title and added help screen entries

--- HelpScreen.java
+++ HelpScreen.java
@@ -8,7 +8,6 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class HelpScreen extends World
 {
-
     /**
      * Constructor for objects of class HelpScreen.
      * 
@@ -18,12 +17,28 @@ public class HelpScreen extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1280, 720, 1); 
         Label title = new Label("Tutorial", 80);
-        Label back = new Label("w: back to main menu", 30);
+        Label back = new Label("q: back to main menu", 30);
+        Label ctrl = new Label("Controls", 60);
+        Label move = new Label("w, a, s, d: move character", 40);
+        Label turn = new Label("mouse movement: gun direction",40);
+        Label auto = new Label("e: toggle auto-shoot", 40);
+        Label targ = new Label("Target", 60);
+        Label shot = new Label("Shoot objects to gain experience \nand upgrade points", 40);
+        Label upgr = new Label("Upgrade tank with upgrade points",40);
+        Label tank = new Label("New tank every lv 15", 40);
         addObject(title, 150, 50);
+        addObject(ctrl, 300, 130);
+        addObject(move, 300, 200);
+        addObject(turn, 300, 250);
+        addObject(auto, 300, 300);
+        addObject(targ, getWidth()-300, 130);
+        addObject(shot, getWidth()-300, 200);
+        addObject(upgr, getWidth()-300, 275);
+        addObject(tank, getWidth()-300, 325);
         addObject(back, getWidth()-300, getHeight()-50);
     }
     public void act(){
-        if(Greenfoot.isKeyDown("w")){
+        if(Greenfoot.isKeyDown("q")){
             TitleScreen titlWorld = new TitleScreen();
             Greenfoot.setWorld(titlWorld);
         }
--- TitleScreen.java
+++ TitleScreen.java
@@ -16,15 +16,15 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1280, 720, 1); 
-        Label title = new Label("A Random Game", 70);
+        Label title = new Label("A Horrible Copy of diep.io", 70);
         Label start = new Label("Press Space to Start", 30);
-        Label help = new Label("Press q for Help",30);
+        Label help = new Label("Press h for Help",30);
         addObject(title, getWidth()/2, getHeight()/2);
         addObject(start, getWidth()/2, getHeight()/2+100);
         addObject(help, getWidth()/2, getHeight()/2+150);
     }
     public void act(){
-        if(Greenfoot.isKeyDown("q")){
+        if(Greenfoot.isKeyDown("h")){
             HelpScreen tutWorld = new HelpScreen();
             Greenfoot.setWorld(tutWorld);
         }
